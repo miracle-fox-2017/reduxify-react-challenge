e9d83364caac101355f12dd76e58619e81a1ae01
Nandira Paturohman <nandirasp@gmail.com>
2017-12-13 16:13:11 +0700

    object assign reduce

--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -6,8 +6,7 @@ const search = (state = stateDefault, action) => {
   switch (action.type) {
     case 'SEARCH_KEYWORD': 
       console.log('masuk 1')
-      state.keyword = action.payload
-      return state
+      return (Object.assign({}, state, {keyword: action.payload}))
     default :
       console.log('masuk default')
       return state
