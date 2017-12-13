441df839646c0086da51190c120fa1226e14c659
Nandira Paturohman <nandirasp@gmail.com>
2017-12-13 20:23:03 +0700

    loading gamedetail

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,23 +6,43 @@ import { connect } from 'react-redux'
 import { gameDetail } from '../actions/gameDetailAction'
 
 class GameDetail extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loading: false
+    }
+  }
+
+
   componentWillMount() {
+    this.setState({
+      loading: true
+    })
     let id = this.props.match.params.id
     axios.get('https://www.giantbomb.com/api/game/'+id+'/?api_key=81b142b95e0dc166df9f0ddc886621c0ec8a3254')
     .then(({data}) => {
       let jsonObj = fastXmlParser.parse(data)
       let gameData = jsonObj.response.results
       this.props.gameDetail(gameData)
+      this.setState({
+        loading: false
+      })
     })
   }
 
   render() {
+    let content = ''
+    if(this.state.loading) {
+      content = <div><h1 className="title">Loading...</h1></div>
+    } else {
+      content = <div className="column is-12 article">
+                  <p className="title">{this.props.gameData.name}</p>
+                  {ReactHtmlParser(this.props.gameData.description || this.props.gameData.deck)}
+                </div>
+    }
     return(
       <div>
-      <p className="title">{this.props.gameData.name}</p>
-      <div className="column is-12 article">
-        {ReactHtmlParser(this.props.gameData.description || this.props.gameData.deck)}
-      </div>
+        {content}
       </div>
     )
   }
