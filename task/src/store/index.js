f9a3d8b776c6de3ee6af299320f32409d0ea2e42
ipunganwar <ipung.anwar1@gmail.com>
2017-12-14 07:52:10 +0700

    logger

--- task/src/store/index.js
+++ task/src/store/index.js
@@ -1,6 +1,13 @@
-import { createStore } from 'redux'
-import articles from '../reducer'  
+import { createStore, applyMiddleware } from 'redux'
+import articles from '../reducer'
+import logger from 'redux-logger' 
 
-const store = createStore(articles)
+const mylogger = (store) => (next) => (action) => {
+    console.log('LOgged Action', action)
+    next(action)
+}
+const store = createStore(articles, 
+    applyMiddleware(logger)
+)
 
 export default store
